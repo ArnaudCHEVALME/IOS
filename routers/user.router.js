cacfe491138479c2035c852b677facd8de2c691f
ArnaudCHEVALME <arnaud.chevalme@proton.me>
2024-01-25 12:24:10 +0100

    Update user router to include new routes

--- routers/user.router.js
+++ routers/user.router.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/user.controller');
 
 module.exports = (app) => {
-  router.get('/', controller.getUserList);
+  router.get('/', controller.getAll);
   router.get('/:user_id', controller.getUserById);
+  router.post('/login', controller.simpleLogin);
 
   app.use('/user', router);
 }
